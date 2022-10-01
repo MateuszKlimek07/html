68a02b6dbd58e1aba21baa591ce88bd3cb084004
MateuszKlimek07 <114807673+MateuszKlimek07@users.noreply.github.com>
2022-10-01 14:46:51 +0200

    Add files via upload

---
+++ makamkm.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Makamkm {
+    public static void main(String[] args){
+        Scanner odczytgodziny = new Scanner(System.in);
+        System.out.println("podaj Godzine");
+        public static void makamkm(String [] args) {
+            int time = 20;
+            if (time < 18) {
+                System.out.println("Dzień Dobry.");
+            } else {
+                System.out.println("Dobry wieczór");
+            }
+        }
+
+    }
+
+    {int i = 0;
+        while (i < 10) {
+            System.out.println("Zmienna i: " + i);
+            i=i+1;
+        }
+    }
+}
+
